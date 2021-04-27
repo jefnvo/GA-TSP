ae220279e2c75c3738dd0d1c864c3f9a1c0bf4a5
Jefferson Freitas Oliveira <jefferson.oliveira@ifood.com.br>
2021-04-27 02:28:33 -0300

    adding more tests

--- src/main/java/Genetic.java
+++ src/main/java/Genetic.java
@@ -4,7 +4,7 @@ public class Genetic {
     public static void main(String[] args) throws IOException {
         TSPReader reader = new TSPReader("../resources/gr21.tsp.txt");
 
-        TSPSolver geneticTSPSolver = new TSPSolver(reader.getNumCities(), 10000, 1000, reader.getDistanceWeight(),0.8,0.1);
+        TSPSolver geneticTSPSolver = new TSPSolver(reader.getNumCities(), 150, 500, reader.getDistanceWeight(),0.8,0.1);
         System.out.println(geneticTSPSolver.geneticSolver());
 
     }
